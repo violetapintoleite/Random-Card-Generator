36f6425f1f80a1fd1b5385900ad3e466990a72b6
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-05-18 11:25:48 +0000

    Added js functions and css relevant classes

--- src/app.js
+++ src/app.js
@@ -8,4 +8,35 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let nipes = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let targetNipe = document.querySelectorAll(".nipe");
+  let targetNumber = document.querySelectorAll(".number");
+  let changeColor = document.querySelector(".color");
+  let triggerButton = document.querySelector(".trigger");
+
+  function loopArray(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  function generateCard() {
+    var newNipe = `${nipes[loopArray(nipes)]}`;
+    var newNumber = `${numbers[loopArray(numbers)]}`;
+    targetNipe.forEach(function(nipe) {
+      if (newNipe === "♥" || newNipe === "♦") {
+        changeColor.style.color = "red";
+      }
+      if (newNipe === "♠" || newNipe === "♣") {
+        changeColor.style.color = "black";
+      }
+      nipe.innerHTML = newNipe;
+    });
+    targetNumber.forEach(function(number) {
+      number.innerHTML = newNumber;
+    });
+  }
+
+  triggerButton.addEventListener("click", generateCard);
 };
